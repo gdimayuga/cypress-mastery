5dc96cb09024bceec82f504212c0b50670d29832
gdimayuga <GDIMAYUGA@VERTERE-GS.COM>
2025-04-08 11:27:47 +0800

    yml v1

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "a5d2r8",
+  experimentalStudio: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
