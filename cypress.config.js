1e2681281d6a49fc43a49dee21859e950c41d6b2
gdimayuga <GDIMAYUGA@VERTERE-GS.COM>
2025-04-08 10:21:01 +0800

    cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
+// npx cypress run --record --key d0457e00-4a25-4686-b7bc-cf5d8deb1aad
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "a5d2r8",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
