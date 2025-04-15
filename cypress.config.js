d2fde5d9dd3dd53026538ea7fa96a1caf7632f61
gdimayuga <GDIMAYUGA@VERTERE-GS.COM>
2025-04-15 14:40:37 +0800

    fixed report page

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,35 @@
 // npx cypress run --record --key b57c8813-d09d-4baa-8d1f-e2ddeb129388
 const { defineConfig } = require("cypress");
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
   projectId: "a5d2r8",
-  experimentalStudio: false,
-  viewportHeight:1080,
-  viewportWidth:1920,
+  experimentalStudio: true,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  //retries: 3,
+  // testIsolation: false,
+  reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     charts: true,
-    reportPageTitle: 'custom-title',
+    reportPageTitle: 'Test Report',
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
   },
   e2e: {
-    testIsolation: true,
+    //testIsolation: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        console.log('Running tests');
+        await beforeRunHook(details);
+      });
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
+    },
   },
 });
