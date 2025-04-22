0f83e1bbffbeb6a4ca1d234c0ab0d5f9a6b5f0dd
gdimayuga <GDIMAYUGA@VERTERE-GS.COM>
2025-04-22 15:38:22 +0800

    api testing: pet-store-api

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 // npx cypress run --record --key b57c8813-d09d-4baa-8d1f-e2ddeb129388
 const { defineConfig } = require("cypress");
 const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
+const dotenv = require("dotenv");
+require("dotenv").config();
+
 module.exports = defineConfig({
   projectId: "a5d2r8",
   experimentalStudio: true,
-  //retries: 3,
+  retries: 3,
   // testIsolation: false,
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
@@ -14,7 +17,14 @@ module.exports = defineConfig({
     inlineAssets: true,
     saveAllAttempts: false,
   },
+  env: {
+    projectName: process.env.PROJECT_NAME || "Cypress Test Automation",
+    environment: process.env.ENVIRONMENT || "QA",
+    API_KEY: process.env.API_KEY,
+    API_BASE_URL: process.env.API_BASE_URL || 'https://petstore.swagger.io/v2',
+  },
   e2e: {
+    baseUrl: process.env.API_BASE_URL || 'https://petstore.swagger.io/v2',
     //testIsolation: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
---
+++ cypress/e2e/api-testing/pet-store-api.cy.js
@@ -0,0 +1,267 @@
+describe('Store API Tests', () => {
+  const updateUser = {
+    "id": 1,
+    "username": "hahaz",
+    "firstName": "string",
+    "lastName": "string",
+    "email": "string",
+    "password": "string",
+    "phone": "string",
+    "userStatus": 0
+  };
+
+  it('GET - Returns pet inventories by status', () => {
+    cy.api({
+      method: 'GET',
+      url: `https://petstore.swagger.io/v2/store/inventory`
+    }).should((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('sold');
+    });
+  });
+
+  it('POST - Place an order for a pet', () => {
+    cy.api({
+      method: 'POST',
+      url: `https://petstore.swagger.io/v2/store/order`,
+      body: {
+        "shipDate": "2025-04-22T04:23:00.976+0000",
+        "status": "placed",
+        "complete": true,
+        "id": 1,
+        "petId": 0,
+      }
+    }).should((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('id', 1); 
+    });
+  });
+
+  it('GET - Find purchase order by ID', () => {
+    cy.api({
+      method: 'GET',
+      url: `https://petstore.swagger.io/v2/store/order/1`
+    }).should((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('id');
+    });
+  });
+
+  it('DELETE - Delete purchase order by ID', () => {
+    cy.api({
+      method: 'POST',
+      url: `https://petstore.swagger.io/v2/store/order`,
+      body: {
+        id: 3,
+        petId: 123,
+        quantity: 1,
+        shipDate: new Date().toISOString(),
+        status: 'placed',
+        complete: true
+      }
+    }).should((response) => {
+      expect(response.status).to.eq(200)
+    })
+    
+    cy.api({
+      method: 'DELETE',
+      url:  `https://petstore.swagger.io/v2/store/order/3`
+    }).should((response) => {
+      expect(response.status).to.eq(200)
+    })
+  })
+});
+
+describe('User API Tests', () => {
+
+  const updateUser = {
+    "id": 1,
+    "username": "hahaz",
+    "firstName": "string",
+    "lastName": "string",
+    "email": "string",
+    "password": "string",
+    "phone": "string",
+    "userStatus": 0
+  };
+
+  it ('POST - Create a list of users with given input array', () => {
+
+    const users = [
+      {
+        "id": 0,
+        "username": "string",
+        "firstName": "string",
+        "lastName": "string",
+        "email": "string",
+        "password": "string",
+        "phone": "string",
+        "userStatus": 0
+      },
+      {
+        "id": 1,
+        "username": "string",
+        "firstName": "string",
+        "lastName": "string",
+        "email": "string",
+        "password": "string",
+        "phone": "string",
+        "userStatus": 0
+      }
+    ]
+
+    cy.api({
+      method: 'POST',
+      url: `https://petstore.swagger.io/v2/user/createWithList`,
+      body: users
+    }).should((response) => {
+      expect(response.status).to.eq(200)
+    })
+  })
+
+  it ('POST - Create a list of users with given input array', () => {
+
+    const users = [
+      {
+        "id": 0,
+        "username": "string",
+        "firstName": "string",
+        "lastName": "string",
+        "email": "string",
+        "password": "string",
+        "phone": "string",
+        "userStatus": 0
+      },
+      {
+        "id": 1,
+        "username": "string",
+        "firstName": "string",
+        "lastName": "string",
+        "email": "string",
+        "password": "string",
+        "phone": "string",
+        "userStatus": 0
+      }
+    ]
+
+    cy.api({
+      method: 'POST',
+      url: `https://petstore.swagger.io/v2/user/createWithArray`,
+      body: users
+    }).should((response) => {
+      expect(response.status).to.eq(200)
+    })
+  })
+
+  it('GET - Get user by user name', () => {
+
+    cy.api({
+      method: 'POST',
+      url: `https://petstore.swagger.io/v2/user`,
+      body: {
+        "id": 0,
+        "username": "haha",
+        "firstName": "string",
+        "lastName": "string",
+        "email": "string",
+        "password": "string",
+        "phone": "string",
+        "userStatus": 0
+      }
+    })
+
+    cy.api({
+      method: 'GET',
+      url: `https://petstore.swagger.io/v2/user/haha`
+    }).should((response) => {
+      expect(response.status).to.eq(200)
+    })
+  })
+
+  it('PUT - Updated user', () => {
+
+    cy.api({
+      method: 'POST',
+      url: `https://petstore.swagger.io/v2/user`,
+      body: {
+        "id": 0,
+        "username": "haha",
+        "firstName": "string",
+        "lastName": "string",
+        "email": "string",
+        "password": "string",
+        "phone": "string",
+        "userStatus": 0
+      }
+    })
+
+    cy.api({
+      method: 'PUT',
+      url: `https://petstore.swagger.io/v2/user/haha`,
+      body: updateUser
+    }).should((response) => {
+      expect(response.status).to.eq(200)
+    })
+
+    cy.api({
+      method: 'GET',
+      url: `https://petstore.swagger.io/v2/user/haha`
+    }).should((response) => {
+      expect(response.status).to.eq(200);
+      return response.body.username === updateUser.username;
+    })
+  })
+
+  it('DELETE - Delete User', () => {
+    cy.api({
+      method: 'POST',
+      url: `https://petstore.swagger.io/v2/user`,
+      body: {
+        "id": 0,
+        "username": "haha",
+        "firstName": "string",
+        "lastName": "string",
+        "email": "string",
+        "password": "string",
+        "phone": "string",
+        "userStatus": 0
+      }
+    }).then(() => {
+      cy.api({
+        method: 'DELETE',
+        url: `https://petstore.swagger.io/v2/user/haha`
+      }).then((response) => {
+        cy.log('DELETE Response: ', JSON.stringify(response));
+          expect(response.status).to.eq(200);
+      });
+        cy.wait(1000); 
+    });
+  });
+
+  it('GET - Logs user into the system', () => {
+
+    const queryParams = {
+      username: 'haha', 
+      password: 'password' 
+    };
+
+    cy.api({
+      method: 'GET',
+      url: `https://petstore.swagger.io/v2/user/login`,
+      qs: queryParams
+    }).should((response) => {
+      expect(response.status).to.eq(200)
+    })
+  })
+
+  it('GET - Logs out current logged in user session', () => {
+    cy.api({
+      method: 'GET',
+      url: `https://petstore.swagger.io/v2/user/logout`
+    }).should((response) => {
+      expect(response.status).to.eq(200)
+    })
+  })
+
+});
+
